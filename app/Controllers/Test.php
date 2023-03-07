581f2aefa8d430d2b9aa7bc8d602dd86ceba0848
lblum <lblum@menhir.com.ar>
2023-03-07 10:27:42 -0300

    composer-update

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -4,8 +4,25 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+
 class Test extends BaseController
 {
+
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+
+        // Preload any models, libraries, etc, here.
+
+        // E.g.: $this->session = \Config\Services::session();
+    }
+
+
     public function index()
     {
         // TODO: chequear development
